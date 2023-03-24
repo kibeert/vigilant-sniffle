c340010e8ebff45d97726338cbbc2d93954e62b1
kibet <107929686+kibeert@users.noreply.github.com>
2023-03-24 03:41:47 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    static void fullThrottle() {
+        System.out.println("The car is going as fast as it can");
+    }
+
+    static void Speed(int maxspeed) {
+        System.out.println("max speed is:" + maxspeed);
+    }
+
+    public static void main(String[] args) {
+        fullThrottle();
+        Speed(200);
+
+    }
+}
